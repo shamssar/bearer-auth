82d0f3a45cb2e20c85503887b8b00defdd6190ce
shamssar <nayefshams3@gmail.com>
2022-06-26 16:47:38 +0300

    edit

--- src/routes/getUsers.js
+++ src/routes/getUsers.js
@@ -1,14 +1,13 @@
 'use strict';
 const express = require('express');
 const getUsersRouter=express.Router();
-const {users}=require('../models/users');
+const { users }=require('../models/users');
 const bearerAuth = require('../middlewares/bearer');
 
 getUsersRouter.get('/users',bearerAuth,async(req,res,next)=>{
     try {
-        const userRecords = await users.findAll({});
-        const list = userRecords.map(user => user.username);
-        res.status(200).json(list);
+        const userRecords = await users.findAll();
+        res.status(200).json(userRecords);
       } catch (e) {
         console.error(e);
         next(e);
